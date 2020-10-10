6e6bd34547d8e4ba0064edb54ef872abb1fb4189
EndOfMaster <843698826@qq.com>
2020-10-10 17:41:25 +0800

    添加返回微信订单号

--- src/main/java/com/endofmaster/weixin/pay/WxMerchantPayResponse.java
+++ src/main/java/com/endofmaster/weixin/pay/WxMerchantPayResponse.java
@@ -1,7 +1,16 @@
 package com.endofmaster.weixin.pay;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * @author ZM.Wang
  */
 public class WxMerchantPayResponse extends WxPayResponse {
+
+    @JsonProperty("payment_no")
+    private String paymentNo;
+
+    public String getPaymentNo() {
+        return paymentNo;
+    }
 }
